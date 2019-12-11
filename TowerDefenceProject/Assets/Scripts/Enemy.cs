6a501f82bbc62e0e674ac150af848f6e5fc3413c
CubeMD <markelovdp@gmail.com>
2019-12-11 17:33:23 -0500

    enemy a bit

---
+++ TowerDefenceProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Point target;
+    public float speed;
+    public float hp;
+    public float acceptanceRadius;
+
+    private void Update()
+    {
+        //transform.LookAt(target.transform);
+        transform.Translate(speed * Time.deltaTime * (target.transform.position - transform.position));
+        
+        float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+        
+        if (distanceToTarget < acceptanceRadius)
+        {
+            if (target.nextPoint != null)
+            {
+                target = target.nextPoint;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ TowerDefenceProject/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Point[] startingPoints;
+    public float timeInterval;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPoints = FindObjectsOfType<Point>().Where(x => x.previousPoints.Count == 0).ToArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
